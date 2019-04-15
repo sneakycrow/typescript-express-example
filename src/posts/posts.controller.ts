88ef884f5511e2d11985ba98559bf16694b513ca
Zachary Sohovich <zachsohovich@thebriga.de>
2019-04-15 16:13:50 -0700

    added crud routes for posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,20 +1,12 @@
 import * as express from "express";
+import Controller from "../interfaces/controller.interface";
 import Post from "./post.interface";
+import postModel from "./posts.model";
 
 class PostsController {
     public path = "/posts";
     public router = express.Router();
-
-    // Our array of posts
-    // NOTE: This is temporary data
-    // TODO: Connect to DB and get posts from there
-    private posts: Post[] = [
-        {
-            author: "Zachary",
-            content: "Dolor sit amet",
-            title: "Lorem Ipsum"
-        }
-    ];
+    private post = postModel;
 
     // Initializer (constructor). This is where we initialize routes for example
     constructor() {
@@ -24,19 +16,66 @@ class PostsController {
     // This sets the routes for our controller
     public initializeRoutes() {
         this.router.get(this.path, this.getAllPosts);
-        this.router.post(this.path, this.createAPost);
+        this.router.get(`${this.path}/:id`, this.getPostsById);
+        this.router.put(`${this.path}/:id`, this.modifyPost);
+        this.router.delete(`${this.path}/:id`, this.deletePost);
+        this.router.post(this.path, this.createPost);
     }
 
     // Func for grabbing all posts and returning them
     private getAllPosts = (request: express.Request, response: express.Response) => {
-        response.send(this.posts);
+        this.post.find()
+            .then((posts) => {
+                response.send(posts);
+            });
     }
 
-    // Func for creating a post from the request body
-    private createAPost = (request: express.Request, response: express.Response) => {
-        const post: Post = request.body;
-        this.posts.push(post);
-        response.send(post);
+    // Func for getting a post by its id
+    // NOTE: This doesn't have error handling, so if your id doesn't exist it might have unintended results
+    // TODO: Add error handling
+    private getPostsById = (request: express.Request, response: express.Response) => {
+        const id = request.params.id;
+        this.post.findById(id)
+            .then((post) => {
+                response.send(post);
+            });
+    }
+
+    // Func for modifying a post
+    // NOTE: This doesn't have error handling, so if your id doesn't exist it might have unintended results
+    // TODO: Add error handling
+    private modifyPost = (request: express.Request, response: express.Response) => {
+        const id = request.params.id;
+        const postData: Post = request.body;
+        this.post.findByIdAndUpdate(id, postData, { new: true })
+            .then((post) => {
+                response.send(post);
+            });
+    }
+
+    // Func for creating a new post
+    private createPost = (request: express.Request, response: express.Response) => {
+        const postData: Post = request.body;
+        const createdPost = new this.post(postData);
+        createdPost.save()
+            .then((savedPost) => {
+                response.send(savedPost);
+            });
+    }
+
+    // Func for deleteing a post
+    // NOTE: This doesn't have _proper_ error handling. It basically knows to fail when there's an error
+    // TODO: Customize error handling for different scenarios
+    private deletePost = (request: express.Request, response: express.Response) => {
+        const id = request.params.id;
+        this.post.findByIdAndDelete(id)
+            .then((successResponse) => {
+                if (successResponse) {
+                    response.send(200);
+                } else {
+                    response.send(400);
+                }
+            });
     }
 }
 
